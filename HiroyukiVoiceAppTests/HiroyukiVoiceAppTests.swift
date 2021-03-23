1b9f3212e9a33b0fa884701c21021f3b0307db28
yasinoki39 <yasinoki39@gmail.com>
2021-03-24 06:20:43 +0900

    Initial Commit

---
+++ HiroyukiVoiceAppTests/HiroyukiVoiceAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  HiroyukiVoiceAppTests.swift
+//  HiroyukiVoiceAppTests
+//
+//  Created by SHINGO YANAGIDA on 2021/03/24.
+//
+
+import XCTest
+@testable import HiroyukiVoiceApp
+
+class HiroyukiVoiceAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
