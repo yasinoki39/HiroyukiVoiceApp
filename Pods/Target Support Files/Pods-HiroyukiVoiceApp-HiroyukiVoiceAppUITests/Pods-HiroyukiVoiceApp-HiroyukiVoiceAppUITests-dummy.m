b1d2a656fdf2b902d03c94af9a827cd4dfe3ab31
yasinoki39 <yasinoki39@gmail.com>
2022-05-08 03:43:39 +0900

    .gitignoreを編集

---
+++ Pods/Target Support Files/Pods-HiroyukiVoiceApp-HiroyukiVoiceAppUITests/Pods-HiroyukiVoiceApp-HiroyukiVoiceAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HiroyukiVoiceApp_HiroyukiVoiceAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HiroyukiVoiceApp_HiroyukiVoiceAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-HiroyukiVoiceApp/Pods-HiroyukiVoiceApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HiroyukiVoiceApp : NSObject
+@end
+@implementation PodsDummy_Pods_HiroyukiVoiceApp
+@end
